9bac276f030b44a9528567e79fc60c6640a6274b
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-26 07:40:36 +0500

    js functionality

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let selectField = document.getElementById("selectField");
 let selectText = document.getElementById("selectText");
 let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+let arrowIcon = document.getElementById("arrowIcon");
+
+selectField.onclick = function(){
+    list.classList.toggle("hide");
+}
 
 for(option of options){
     option.onclick = function(){
         selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
     }
 }
